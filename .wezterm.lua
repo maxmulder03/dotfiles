ea53a403a9fd058f361845c3f40b4e21d958c979
Max Mulder <maxmulder03@gmail.com>
2024-11-14 12:24:06 -0500

    wezterm tmux keybindings

--- .wezterm.lua
+++ .wezterm.lua
@@ -37,7 +37,88 @@ config.keys = {
   },
 }
 
+-- tmux-esq congig
+config.leader = { key = "g", mods = "CTRL", timeout_milliseconds = 2000 }
+config.keys = {
+  {
+    mods = "LEADER",
+    key = "c",
+    action = wezterm.action.SpawnTab "CurrentPaneDomain",
+  },
+  {
+    mods = "LEADER",
+    key = "x",
+    action = wezterm.action.CloseCurrentPane { confirm = true }
+  },
+  {
+    mods = "LEADER",
+    key = "b",
+    action = wezterm.action.ActivateTabRelative(-1)
+  },
+  {
+    mods = "LEADER",
+    key = "n",
+    action = wezterm.action.ActivateTabRelative(1)
+  },
+  {
+    mods = "LEADER",
+    key = "'",
+    action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }
+  },
+  {
+    mods = "LEADER",
+    key = "-",
+    action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" }
+  },
+  {
+    mods = "LEADER",
+    key = "h",
+    action = wezterm.action.ActivatePaneDirection "Left"
+  },
+  {
+    mods = "LEADER",
+    key = "j",
+    action = wezterm.action.ActivatePaneDirection "Down"
+  },
+  {
+    mods = "LEADER",
+    key = "k",
+    action = wezterm.action.ActivatePaneDirection "Up"
+  },
+  {
+    mods = "LEADER",
+    key = "l",
+    action = wezterm.action.ActivatePaneDirection "Right"
+  },
+  {
+    mods = "LEADER",
+    key = "LeftArrow",
+    action = wezterm.action.AdjustPaneSize { "Left", 5 }
+  },
+  {
+    mods = "LEADER",
+    key = "RightArrow",
+    action = wezterm.action.AdjustPaneSize { "Right", 5 }
+  },
+  {
+    mods = "LEADER",
+    key = "DownArrow",
+    action = wezterm.action.AdjustPaneSize { "Down", 5 }  
+  },
+  {
+    mods = "LEADER",
+    key = "UpArrow",
+    action = wezterm.action.AdjustPaneSize { "Up", 5 }  
+  },
+}
 
-
+-- Mod + Num to switch to active tab 
+for i = 0, 9 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "LEADER",
+    action = wezterm.action.ActivateTab(i),
+  })
+end 
 
 return config
